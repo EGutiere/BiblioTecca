f47214fc9fbb99b6238aba9b315d17a01133cf7b
Estevaogut <estevaorg@gmail.com>
2016-06-30 20:49:46 -0300

    v1.1

---
+++ BiblioTecca/BiblioTecca/DAL/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioTecca.Model;
+
+namespace BiblioTecca.DAL
+{
+    class Context : DbContext
+    {
+        public Context()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }        
+        public DbSet<Livro> Livros { get; set; }
+        
+    }
+}
