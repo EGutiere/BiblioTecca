09a01242a00c1eaa8ec29a1495e6e3604cc9ee94
Estevaogut <estevaorg@gmail.com>
2016-06-30 20:57:34 -0300

    v1.2

--- BiblioTecca/BiblioTecca/MainWindow.xaml.cs
+++ BiblioTecca/BiblioTecca/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BiblioTecca
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
