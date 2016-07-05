54a4a2c406dcaf0b1aacacbb69ec40419078ec0f
estevaogut <estevaorg@gmail.com>
2016-07-04 21:54:51 -0300

    v2.2

---
+++ BiblioTecca/BiblioTecca/Model/Locacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioTecca.Model
+{
+    [Table("Locacao")]
+    class Locacao
+    {
+        [Key]
+        public int IdLocacao { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public Livro Livro { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public bool Status { get; set; }
+    }
+}
